63721c9505e0194c77610cfc654fc58d137efbf3
william <accrazor@gmail.com>
2022-06-24 11:25:35 -0600

    created sales poller

--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -1,3 +1,4 @@
+from urllib import response
 import django
 import os
 import sys
@@ -10,7 +11,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_project.settings")
 django.setup()
 
 # Import models from sales_rest, here.
+from sales_rest.models import Automobile
 # from sales_rest.models import Something
+
+def get_vin():
+    response = requests.get("htto")
 
 def poll():
     while True:
