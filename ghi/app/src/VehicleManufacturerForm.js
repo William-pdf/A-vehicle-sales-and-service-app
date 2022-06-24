a70c9334b0c2d67bd3bc36b7d9e913a22dba51b6
william <accrazor@gmail.com>
2022-06-24 12:28:28 -0600

    fixed manufactures form

--- ghi/app/src/VehicleManufacturerForm.js
+++ ghi/app/src/VehicleManufacturerForm.js
@@ -1,71 +1,62 @@
-import React from 'react';
 
-class ManufacturerForm extends React.Component {
+export default class ManufacturerForm extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-    }
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChangeName = this.handleChangeName.bind(this);
+      super(props);
+      this.state = {
+          name: '',
+      }
+      this.handleNameChange = this.handleNameChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
   }
-  async handleSubmit(event) {
-    event.preventDefault();
-    const data = { ...this.state };
-    delete data.hasCreated;
 
-    const url = 'http://localhost:8100/api/manufacturers'
-    const fetchConfig = {
-      method: 'post',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-    const manformResponse = await fetch(url, fetchConfig);
-    if (manformResponse.ok) {
-      const NewManufacturer = await manformResponse.json();
-      console.log(NewManufacturer)
-      this.setState({
-        name: '',
-      })
-    }
+  async handleSubmit(event) {
+      event.preventDefault();
+      const data = { ...this.state };
+      const createManufacturer = 'http://localhost:8100/api/manufacturers/';
+      const fetchConfig = {
+          method: 'post',
+          body: JSON.stringify(data),
+          headers: {
+              'Content-Type': 'application/json',
+          },
+      };
+
+      const response = await fetch(createManufacturer, fetchConfig);
+      if (response.ok) {
+          const newManufacturer = await response.json();
+          console.log(newManufacturer);
+      }
+
+      const cleared = {
+          name: '',
+      }
+      this.setState(cleared);
   }
-  handleChangeName(event) {
-    const value = event.target.value;
-    this.setState({ name: value });
+
+  handleNameChange(event) {
+      const value = event.target.value;
+      this.setState({ name: value });
   }
+
 
   render() {
-    let messageClasses = 'alert alert-success d-none mb-0';
-    let formClasses = '';
-    if (this.state.hasCreated) {
-      messageClasses = 'alert alert-success mb-0';
-      formClasses = 'd-none';
-    }
-
-    return (
-      <>
-        <div className='row'>
-          <div className='col col-sm-auto'>
-            <form className='{formClasses}' onSubmit={this.handleSubmit} id="create-manufacturer-form">
-              <h1 className='card-title'>Enter New Manufacturer</h1>
-              <div className="col">
-                <div className="form-floating mb-3">
-                  <input onChange={this.handleChangeName} required placeholder="Name" type="text" id="name" name="name" className="form-control" />
-                  <label htmlFor="">Manufacturer Name</label>
-                </div>
+      return (
+          <>
+              <div className="row">
+                  <div className="offset-3 col-6">
+                      <div className="shadow p-4 mt-4">
+                          <h1>Create a new manufacturer</h1>
+                          <form onSubmit={this.handleSubmit} id="create-salerep-form">
+                              <div className="form-floating mb-3">
+                                  <input onChange={this.handleNameChange} value={this.state.name} placeholder="Name" required type="text" name="name" id="name" className="form-control" />
+                                  <label htmlFor="name">Name</label>
+                              </div>
+                              <button className="btn btn-outline-dark">Create</button>
+                          </form>
+                      </div>
+                  </div>
               </div>
-              <button className="btn btn-lg btn-primary">Add</button>
-            </form>
-            <div className={messageClasses} id="success-message">
-              Manufacturer Added
-            </div>
-          </div>
-        </div>
-      </>
-    )
+          </>
+      )
   }
 }
-export default ManufacturerForm;
-
